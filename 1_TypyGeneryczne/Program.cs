33e3f0837205b61cac1000fc09460bc5a8a914a5
Jarosław Olszewski <oljar@tlen.pl>
2022-07-22 15:29:15 +0200

    1.4  Kolejka Kołowa

--- 1_TypyGeneryczne/Program.cs
+++ 1_TypyGeneryczne/Program.cs
@@ -6,7 +6,29 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+
+            while (true)
+            {
+
+                var wartosc = 0.0;
+                var wartoscwejsciowa = Console.ReadLine();
+                if (double.TryParse(wartoscwejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+
+
+            Console.WriteLine("W kolejxce jest : ");
+
+            while (!kolejka.JestPusty)
+            {
+                Console.WriteLine("\t\t" + kolejka.Czytaj());
+
+            }
         }
     }
 }
